57a328963fbe1f42577882058f7f7c40114db722
keith <keith@linux-pop>
2026-07-06 18:32:44 +0800

    tagged Google Ads

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -18,7 +18,7 @@ export default function SiteLayout({
         strategy="lazyOnload"
       />
       <Script id="ga4-init" strategy="lazyOnload">
-        {`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-4NR3D3JVVL');`}
+        {`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-4NR3D3JVVL');gtag('config','AW-716364762');`}
       </Script>
       <Script id="microsoft-clarity" strategy="lazyOnload">
         {`(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window,document,"clarity","script","m2654ikl3f");`}
